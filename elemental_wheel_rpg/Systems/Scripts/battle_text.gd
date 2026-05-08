a95d5a094679243139b7c12471a4f4537dc1b211
William Harris <william.harris108@gmail.com>
2026-05-08 17:28:41 -0500

    Added arrow graphics

--- elemental_wheel_rpg/Systems/Scripts/battle_text.gd
+++ elemental_wheel_rpg/Systems/Scripts/battle_text.gd
@@ -2,6 +2,9 @@ extends RichTextLabel
 
 @export var text_dictionary = {}
 
+signal player_won
+signal player_lost
+
 
 func _ready():
 	visible_ratio = 0.0
@@ -11,11 +14,14 @@ func type_text() -> void:
 	
 	var tween = create_tween()
 	tween.tween_property(self, "visible_ratio", 1.0, 1.0).from(0.0)
+	await tween.finished
 
 func _on_player_lost() -> void:
 	text = text_dictionary[&"Player Defeated"].text_to_render
 	type_text()
+	player_lost.emit()
 
 func _on_player_won() -> void:
 	text = text_dictionary[&"Enemy Defeated"].text_to_render
 	type_text()
+	player_won.emit()
--- elemental_wheel_rpg/Systems/Scripts/wheel_display.gd
+++ elemental_wheel_rpg/Systems/Scripts/wheel_display.gd
@@ -4,6 +4,10 @@ extends Control
 @onready var wheel = $Wheel
 @onready var damage_text = %Damage
 @onready var player_moves = %"Player Moves"
+@onready var pos_indicator = $"Positive Indicator"
+@onready var neg_indicator = $"Negative Indicator"
+
+@export var enemy: Entity
 
 
 func _on_wheel_puzzle_finished() -> void:
@@ -21,3 +25,18 @@ func _on_prototype_battle_scene_start_wheel() -> void:
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "Transition to Wheel":
 		wheel.open_wheel()
+
+
+func _on_wheel_new_dir_selected() -> void:
+	var element = wheel.current_direction / 90
+	var entity_resistances = enemy.entity_data.resistances
+	if entity_resistances[element] == GlobalEnums.Resistance.RESISTANT:
+		neg_indicator.visible = true
+		pos_indicator.visible = false
+	elif entity_resistances[element] == GlobalEnums.Resistance.WEAK:
+		neg_indicator.visible = false
+		pos_indicator.visible = true
+	else:
+		neg_indicator.visible = false
+		pos_indicator.visible = false
+		
